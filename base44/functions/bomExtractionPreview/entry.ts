23587a71aac1eac35d1342aa5b9121ce169075d8
amr.m.barakat <amr.m.barakat@gmail.com>
2026-05-11 09:15:34 +0000

    File changes

--- base44/functions/bomExtractionPreview/entry.ts
+++ base44/functions/bomExtractionPreview/entry.ts
@@ -272,6 +272,7 @@ Deno.serve(async (req) => {
     );
 
     const rawItems = batchResults.flat();
+    const sheetName = '';
 
     // Normalize non-panel items
     const normalizedItems = rawItems
